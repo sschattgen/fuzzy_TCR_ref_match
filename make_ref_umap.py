42f7741b29909e305474f17df9678e457c23bc51
Stefan <sschattgen@gmail.com>
2022-12-21 11:48:31 -0600

    little trimming of the db

--- make_ref_umap.py
+++ make_ref_umap.py
@@ -1,8 +1,8 @@
 import os
-import conga
 import pandas as pd
 import numpy as np
 from sknetwork.utils import KNNDense
+from sklearn.decomposition import KernelPCA
 from sknetwork.clustering import Louvain
 import umap
 from conga.preprocess import calc_tcrdist_matrix_cpp
@@ -13,6 +13,7 @@ os.chdir(working_dir)
 # in/out
 organism = 'human'
 df = pd.read_csv('data/new_paired_tcr_db_for_matching_nr.tsv', sep='\t')
+df = df[df.db == 'vdjdb']
 new_cols = {"va":"va_gene", "ja" :"ja_gene", "vb":"vb_gene", "jb" :"jb_gene"}
 df = df.rename(columns = new_cols)
 # tuples of tuples with tcr info
@@ -22,16 +23,21 @@ tcrs = [((l.va_gene, l.ja_gene, l.cdr3a),
 # calc tcrdist with C++ TCRdist calculator, which needs to be compiled
 D_cpp = calc_tcrdist_matrix_cpp(tcrs, organism)
 
+#smooth with kPCA
+transformer = KernelPCA(n_components=50, kernel='linear')
+tcrdist_mat_kpc = transformer.fit_transform(D_cpp)
+    
 """
 neighbor graph, clustering, and embedding
 """
 louvain = Louvain()
 k = D_cpp.shape[0] // 100
 knn = KNNDense(n_neighbors=k)
-adjacency = knn.fit_transform(D_cpp)
+adjacency = knn.fit_transform(tcrdist_mat_kpc)
 labels = louvain.fit_transform(adjacency)
 reducer = umap.UMAP()
-embedding = reducer.fit_transform(D_cpp)
+embedding = reducer.fit_transform(tcrdist_mat_kpc)
+
 umap_df = (pd.DataFrame(embedding).
  rename({0:'UMAP1', 1:'UMAP2'}, axis=1) 
  )
